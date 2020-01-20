01a746f2d758a029dab1cb5f84c29d900cb59c84
TRAVIS TRAN <duytantran2710@gmail.com>
2020-01-19 20:21:09 -0800

    add sort dir for sorting algorithms

---
+++ sort/sort.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sort.h"
+
+void merge_sort(int a[], size_t size) {
+
+}
---
+++ sort/sort.h
@@ -0,0 +1,5 @@
+#ifndef SORT_H
+#define SORT_H
+// MergeSort(A)
+void merge_sort(int a[], size_t size);
+#endif
